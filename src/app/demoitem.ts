a83d920d87ee73c0310c3756c2ed3b7ac5aca2c5
Nitin Manocha <31987675+nitinmanocha16@users.noreply.github.com>
2019-03-31 20:35:01 +0530

    Update demoitem.ts

--- src/app/demoitem.ts
+++ src/app/demoitem.ts
@@ -1,14 +1,14 @@
 import { Item } from './item';
 
 export const products: Item[] = [
-  { id: 1, name: 'Iphone 7', price: '500$' },
+  { id: 1, name: 'iphone 7', price: '500$' },
   { id: 2, name: 'iphone 8', price: '600$' },
   { id: 3, name: 'iphone X', price: '1000$' },
   { id: 4, name: 'iphone Xs', price: '1200$' },
   { id: 5, name: 'iphone Xr', price: '1500$' },
   { id: 6, name: 'iwatch', price: '500$' },
   { id: 7, name: 'imac', price: '800$' },
-  { id: 8, name: 'mac air', price: '1100$' },
-  { id: 9, name: 'mac pro', price: '1200$' },
+  { id: 8, name: 'Macbook Air', price: '1100$' },
+  { id: 9, name: 'Macbook Pro', price: '1200$' },
   { id: 10, name: 'ipad', price: '800$' }
 ];
